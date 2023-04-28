ad6a873b6abbe6714783e91ec0891196c10e1fb3
fivge <luanxingtong@gmail.com>
2023-04-28 15:02:28 +0800

    feat(rollup): update config

--- rollup/rollup.config.js
+++ rollup/rollup.config.js
@@ -5,15 +5,17 @@ export default {
   input: "src/main.js",
   output: [
     {
-      file: "dist/bundle.js",
-      format: "cjs"
+      format: "cjs",
+      dir: "dist/"
     },
-    {
-      file: "bundle.min.js",
-      format: "iife",
-      name: "version",
-      plugins: [terser()]
-    }
+    // [!] Error: UMD and IIFE output formats are not supported for code-splitting builds
+    // {
+    //   // file: "bundle.min.js",
+    //   format: "iife",
+    //   name: "version",
+    //   // plugins: [terser()],
+    //   dir: "dist/"
+    // }
   ],
   plugins: [json()]
 };
