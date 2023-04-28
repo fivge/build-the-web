6c02a26f170d7efddb51938b928440da4382b523
fivge <luanxingtong@gmail.com>
2023-04-28 14:30:57 +0800

    feat: update to webpack5

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,25 +1,9 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: '../@source/web/src/index.js',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"]
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ["file-loader"]
-      }
-    ]
-  }
 };
